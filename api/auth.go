4fa1e3f1baf867ba58a9ae4ca2382515e301a37b
Scott Raio <scottraio@gmail.com>
2025-04-28 17:27:51 -0700

    nameField for algolia contacts

--- api/auth.go
+++ api/auth.go
@@ -3,16 +3,17 @@ package api
 import (
 	"net/http"
 
-	model "github.com/Proluxe/proluxe-common-api/model"
 	"github.com/Proluxe/proluxe-common-api/util"
 	"github.com/gin-gonic/gin"
 )
 
 func GET_VERIFY_EMAIL(c *gin.Context, App *util.App) {
-	client := App.SF.Client
-	email := c.Param("email")
+	// client := App.SF.Client
+	// email := c.Param("email")
 
-	resp := model.VerifyEmail(client, email)
+	// resp := model.VerifyEmail(client, email)
 
-	c.JSON(http.StatusOK, resp)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Email verification is not implemented yet.", // "data":    resp,
+	})
 }
